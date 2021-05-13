8cbd167f1d09db69191174a90e3630a10e9814be
alex <rootssl@mail.ru>
2021-05-13 19:19:31 +0300

    arrow function, declaration function, arrow function

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 //ARROW FUNCTIONS
 
-const sayConsole = (message) =>{
-    console.log(message);
+console.log( calc(5, 5) );
+
+
+let getAccess = function(){        //expression function --start
+    let string = "you are admin";
+    return string;
 };
 
+console.log(getAccess());            //expression function --end
+
+
+const sayConsole = (message) =>{     //arrow function start     
+    console.log(message);
+};                                  //arrow function end 
+
 sayConsole("WORKED");
+
+function calc(a, b){                //declaration function start
+    return (a + b);
+}                                       //declaration function end
+
+console.log( calc(2, 3) );
+
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+// console.log( ret() );
